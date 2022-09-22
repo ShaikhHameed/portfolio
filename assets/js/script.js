44c4a03fb4ddee612f4d487473d54a70bb34a881
ShaikhHameed <94100930+ShaikhHameed@users.noreply.github.com>
2022-09-23 00:02:31 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+   
+
+  $(function() {
+    $('#selector').mouseenter(function() {
+      $('.work-img-box').fadeIn("fast");
+    });
+  });
+
+  $(function() {
+    $('.no-img-box').mouseenter(function() {
+      $('.work-img-box').fadeOut("fast");
+    });
+  });
+
+   
+  $('.circle-icon').click(function(){
+      $('.nav-link-box').toggleClass('active');
+  });
+  
+  $('.mob-navlink').click(function(){
+    $('.nav-link-box').removeClass('active');
+  })
+
+
+
+  $('.move-img').hide();
+
+    
+      $('.work-card').mouseenter(function(){
+        var $imgsrc =  $(this).children('.work-img').attr("src");
+        $('.move-img').fadeIn("slow"); 
+       $('.move-img').attr("src",$imgsrc);
+
+    });
+
+
+
+
+    var $circle = $('#move-box');
+
+function moveCircle(e) {
+	TweenLite.to($circle, 0.5, {
+    css: {
+      left: (e.pageX + 50),
+      top: (e.pageY + 20)
+    }
+  });
+}
+
+$(window).on('mousemove', moveCircle);
+
+
+
+
+    
+
+
+});
